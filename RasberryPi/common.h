5aaae96c974332bd39f36bd60c6c0b457c0a2e44
Jihoon <67849875+dwgns@users.noreply.github.com>
2023-12-11 13:43:13 +0900

    Update common.h

--- RasberryPi/common.h
+++ RasberryPi/common.h
@@ -9,6 +9,7 @@ extern int share_var;
 extern pthread_mutex_t mutex;
 extern unsigned int adc_slave_addr;
 extern const char* i2c_dev;
+extern unsigned int gas_data;
 
 
 extern void *temperatureSensorFun(void *); // 온습도 센서 쓰레드 함수
